89c7d4eef821bb0e65074ebb42b231eb50c89f8b
India Abbott <indiaabbott@hotmail.com>
2021-04-01 10:39:51 +0100

    adding get request to query db for hourly dogs - test

--- server.ts
+++ server.ts
@@ -31,11 +31,10 @@ app.get("/dogs", async (req, res) => {
   res.json(dogs.rows);
 });
 
-// app.get("/votingtime", async (req, res) => {
-//   const {start_time, end_time} = req.body;
-//   const dogs = await client.query('SELECT dog, COUNT(*) FROM dogs_table WHERE vote_time BETWEEN [$1] AND [$2] GROUP BY dog ORDER BY COUNT(*) DESC', [start_time, end_time]);
-//   res.json(dogs.rows);
-// });
+app.get("/dogshour", async (req, res) => {
+   const dogs = await client.query('SELECT dog, COUNT(*) FROM dogs_table WHERE vote_time BETWEEN NOW() - INTERVAL `20 MINUTES` AND NOW() GROUP BY dog ORDER BY COUNT(*) DESC');
+   res.json(dogs.rows);
+ });
 
 app.post("/dogs", async (req, res) => {
   const {dog} = req.body;
